12fefe536c34edde05471eb3d12f6b30c5a5de6b
Andreas Holstenson <a@holstenson.se>
2011-12-10 12:35:54 +0100

    First version of binary serialization format

---
+++ aurochs-serialization/src/main/java/se/l4/aurochs/serialization/format/BinaryInput.java
@@ -0,0 +1,201 @@
+package se.l4.aurochs.serialization.format;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.common.base.Charsets;
+
+/**
+ * Input for binary format.
+ * 
+ * @author Andreas Holstenson
+ *
+ */
+public class BinaryInput
+	extends AbstractStreamingInput
+{
+	private final InputStream in;
+	
+	private final byte[] buffer;
+	
+	private int peekedByte;
+	
+	public BinaryInput(InputStream in)
+	{
+		this.in = in;
+		buffer = new byte[8];
+		
+		peekedByte = -2;
+	}
+
+	@Override
+	public Token peek()
+		throws IOException
+	{
+		if(peekedByte == -2)
+		{
+			peekedByte = in.read();
+		}
+		
+		switch(peekedByte)
+		{
+			case -1:
+				return null;
+			case BinaryOutput.TAG_KEY:
+				return Token.KEY;
+			case BinaryOutput.TAG_OBJECT_START:
+				return Token.OBJECT_START;
+			case BinaryOutput.TAG_OBJECT_END:
+				return Token.OBJECT_END;
+			case BinaryOutput.TAG_LIST_START:
+				return Token.LIST_START;
+			case BinaryOutput.TAG_LIST_END:
+				return Token.LIST_END;
+			default:
+				return Token.VALUE;
+		}
+	}
+
+	@Override
+	protected Token next0()
+		throws IOException
+	{
+		Token current = peek();
+		if(current == Token.KEY || current == Token.VALUE)
+		{
+			// Read actual data of keys and values
+			readValue();
+		}
+		
+		peekedByte = in.read();
+		
+		return current;
+	}
+	
+	private void readBuffer(int len)
+		throws IOException
+	{
+		int read = in.read(buffer, 0, len);
+		if(read != len)
+		{
+			throw new EOFException("Expected to read " + len + " bytes, but could only read " + len);
+		}
+	}
+	
+	private double readDouble()
+		throws IOException
+	{
+		readBuffer(8);
+		long value = ((long) buffer[0] & 0xff) |
+			((long) buffer[1] & 0xff) << 8 |
+			((long) buffer[2] & 0xff) << 16 |
+			((long) buffer[3] & 0xff) << 24 |
+			((long) buffer[4] & 0xff) << 32 |
+			((long) buffer[5] & 0xff) << 40 |
+			((long) buffer[6] & 0xff) << 48 |
+			((long) buffer[7] & 0xff) << 56;
+		
+		return Double.longBitsToDouble(value);
+	}
+	
+	private float readFloat()
+		throws IOException
+	{
+		readBuffer(4);
+		int value = (buffer[0] & 0xff) |
+			(buffer[1] & 0xff) << 8 |
+			(buffer[2] & 0xff) << 16 |
+			(buffer[3] & 0xff) << 24;
+		
+		return Float.intBitsToFloat(value);
+	}
+	
+	private int readInteger()
+		throws IOException
+	{
+		int shift = 0;
+		int result = 0;
+		while(shift < 32)
+		{
+			final byte b = (byte) in.read();
+			result |= (int) (b & 0x7F) << shift;
+			if((b & 0x80) == 0) return result;
+			
+			shift += 7;
+		}
+		
+		throw new EOFException("Invalid integer");
+	}
+	
+	private long readLong()
+		throws IOException
+	{
+		int shift = 0;
+		long result = 0;
+		while(shift < 64)
+		{
+			final byte b = (byte) in.read();
+			result |= (int) (b & 0x7F) << shift;
+			if((b & 0x80) == 0) return result;
+			
+			shift += 7;
+		}
+		
+		throw new EOFException("Invalid long");
+	}
+	
+	private String readString()
+		throws IOException
+	{
+		int length = readInteger();
+		byte[] buffer = new byte[length];
+		int read = in.read(buffer);
+		
+		if(read != length)
+		{
+			throw new EOFException("Stream ended before entire string was sent");
+		}
+		
+		return new String(buffer, Charsets.UTF_8);
+	}
+	
+	
+	private void readValue()
+		throws IOException
+	{
+		switch(peekedByte)
+		{
+			case BinaryOutput.TAG_BOOLEAN:
+				int b = in.read();
+				setValue(b == 1);
+				break;
+			case BinaryOutput.TAG_DOUBLE:
+				setValue(readDouble());
+				break;
+			case BinaryOutput.TAG_FLOAT:
+				setValue(readFloat());
+				break;
+			case BinaryOutput.TAG_INT:
+				int i = readInteger();
+				i = (i >>> 1) ^ -(i & 1);
+				setValue(i);
+				break;
+			case BinaryOutput.TAG_LONG:
+				long l = readLong();
+				l = (l >>> 1) ^ -(l & 1);
+				setValue(l);
+				break;
+			case BinaryOutput.TAG_NULL:
+				setValue(null);
+				break;
+			case BinaryOutput.TAG_KEY:
+			case BinaryOutput.TAG_STRING:
+				setValue(readString());
+				break;
+			default:
+				throw new IOException("Unexpected value type, no idea what to do (type was " + peekedByte + ")");
+		}
+		
+	}
+}
---
+++ aurochs-serialization/src/test/java/se/l4/aurochs/serialization/format/BinaryTest.java
@@ -0,0 +1,232 @@
+package se.l4.aurochs.serialization.format;
+
+import static junit.framework.Assert.*;
+import static se.l4.aurochs.serialization.format.StreamingInput.Token.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the binary format. Tests by first writing some values and then
+ * checking that it is possible to read the serialized stream.
+ * 
+ * @author Andreas Holstenson
+ *
+ */
+public class BinaryTest
+{
+	private ByteArrayOutputStream out;
+	private BinaryOutput output;
+	
+	@Test
+	public void emptyObject()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, OBJECT_END);
+	}
+	
+	@Test
+	public void emptyList()
+		throws IOException
+	{
+		output.writeListStart("");
+		output.writeListEnd("");
+		
+		assertStream(LIST_START, LIST_END);
+	}
+	
+	@Test
+	public void singleNull()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.writeNull("name");
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", null);
+	}
+	
+	@Test
+	public void singleInt()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", 12);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", 12);
+	}
+	
+	@Test
+	public void singleLong()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", 12l);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", 12l);
+	}
+	
+	@Test
+	public void singleNegativeLong()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", -12l);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", -12l);
+	}
+	
+	@Test
+	public void singleBoolean()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", true);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", true);
+	}
+	
+	@Test
+	public void singleFloat()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", 3.14f);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", 3.14f);
+	}
+	
+	@Test
+	public void singleDouble()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("name", 89765.0);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, OBJECT_END);
+		assertStreamValues("name", 89765.0);
+	}
+	
+	@Test
+	public void severalObjectValues()
+		throws IOException
+	{
+		output.writeObjectStart("");
+		output.write("key1", "value1");
+		output.write("key2", 12);
+		output.writeObjectEnd("");
+		
+		assertStream(OBJECT_START, KEY, VALUE, KEY, VALUE, OBJECT_END);
+		assertStreamValues("key1", "value1", "key2", 12);
+	}
+	
+	@Test
+	public void listWithSeveralValues()
+		throws IOException
+	{
+		output.writeListStart("");
+		output.write("entry", "value");
+		output.write("entry", 74749);
+		output.writeListEnd("");
+		
+		assertStream(LIST_START, VALUE, VALUE, LIST_END);
+		assertStreamValues("value", 74749);
+	}
+	
+	@Before
+	public void beforeTest()
+	{
+		out = new ByteArrayOutputStream();
+		output = new BinaryOutput(out);
+	}
+	
+	protected StreamingInput createInput()
+	{
+		return new BinaryInput(new ByteArrayInputStream(out.toByteArray()));
+	}
+	
+	/**
+	 * Assert that the stream contains the specified tokens.
+	 * 
+	 * @param in
+	 * @param tokens
+	 * @throws IOException
+	 */
+	protected void assertStream(StreamingInput.Token... tokens)
+		throws IOException
+	{
+		StreamingInput in = createInput();
+		int i = 0;
+		while(in.peek() != null)
+		{
+			StreamingInput.Token t = in.next();
+			if(i == tokens.length)
+			{
+				fail("Did not expect more tokens, but got " + t);
+			}
+			else if(t != tokens[i++])
+			{
+				fail("Token at " + (i-1) + " was expected to be " + tokens[i-1] + ", but found " + t);
+			}
+		}
+		
+		if(i < tokens.length)
+		{
+			fail("Did not read all tokens, expected " + tokens.length + " but only read " + i);
+		}
+	}
+	
+	/**
+	 * Assert that KEY and VALUE tokens contain the specified values.
+	 * 
+	 * @param in
+	 * @param values
+	 * @throws IOException
+	 */
+	protected void assertStreamValues(Object... values)
+		throws IOException
+	{
+		StreamingInput in = createInput();
+		int i = 0;
+		while(in.peek() != null)
+		{
+			StreamingInput.Token t = in.next();
+			switch(t)
+			{
+				case KEY:
+				case VALUE:
+					if(i == values.length)
+					{
+						fail("Did not expect more values, but got " + in.getValue());
+					}
+					
+					assertEquals(values[i++], in.getValue());
+					break;
+			}
+		}
+		
+		if(i < values.length)
+		{
+			fail("Did not read all values, expected " + values.length + " but only read " + i);
+		}
+	}
+}
